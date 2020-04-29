bb29d0861970e01e67e9b062a72a4ac278f8ed81
Eduardo <00092117@uca.edu.sv>
2020-04-29 00:29:12 -0600

    Initial commit

---
+++ src/main/java/com/uca/capas/controller/ProductController.java
@@ -0,0 +1,15 @@
+package com.uca.capas.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+
+import com.uca.capas.domain.Product;
+
+@Controller
+public class ProductController {
+
+	private List<Product> products = new ArrayList<Product>();
+	
+}
