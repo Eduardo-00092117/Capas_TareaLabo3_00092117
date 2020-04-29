b0aa4dc155deb5b95312169598efba9dbbc23ab0
Eduardo <00092117@uca.edu.sv>
2020-04-29 00:51:56 -0600

    Se hizo el diseño del formulario con los datos del producto

--- src/main/java/com/uca/capas/controller/ProductController.java
+++ src/main/java/com/uca/capas/controller/ProductController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.uca.capas.domain.Product;
 
@@ -11,5 +13,25 @@ import com.uca.capas.domain.Product;
 public class ProductController {
 
 	private List<Product> products = new ArrayList<Product>();
+	
+	@GetMapping("/producto")
+	public ModelAndView producto() {
+		ModelAndView mav = new ModelAndView();
+		
+		products.clear();
+		
+		products.add(new Product(0, "WWZ", 20));
+		products.add(new Product(1, "Paladins", 15));
+		products.add(new Product(2, "GRID 2", 5));
+		products.add(new Product(3, "Tomb Raider", 60));
+		products.add(new Product(4, "Dirt 3", 34));
+		products.add(new Product(5, "Left 4 Dead 2", 42));
+		
+		mav.setViewName("productos");
+		mav.addObject("producto", products);
+		mav.addObject("product", new Product());
+		
+		return mav;
+	}
 	
 }
